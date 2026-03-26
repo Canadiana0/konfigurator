b8f87afe8b992302101e52298695d9a0b0685405
Canadiana0 <prodej@canadiana.cz>
2026-03-26 10:05:36 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Hydropool konfigurátor",
+  description: "Prodejní konfigurátor Canadiana",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="cs">
+      <body>{children}</body>
+    </html>
+  );
+}
